65343071a3dad4cac7c72196279e35aa529983fc
Iha Moritaka <e153316@ie.u-ryukyu.ac.jp>
2017-11-13 18:23:42 +0900

    report3 step0 modif

--- src/main/jp/ac/uryukyu/ie/e153316/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e153316/Enemy.java
@@ -47,6 +47,9 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
+        if(dead){
+            damage = 0;
+        }
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
     }
